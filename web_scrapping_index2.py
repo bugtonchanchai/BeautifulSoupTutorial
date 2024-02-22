871ad1ab29ae337ea8aa7d2d9040d37f0e93eec0
Chanchai Thaiyanon <chanchai.thaiyanon@outlook.com>
2024-02-22 20:40:11 +0700

    Find attribute

--- web_scrapping_index2.py
+++ web_scrapping_index2.py
@@ -3,5 +3,5 @@ from bs4 import BeautifulSoup
 with open("index2.html", "r") as f:
     doc = BeautifulSoup(f, "html.parser")
 
-tags = doc.find_all(["p", "div", "li"])
+tags = doc.find_all(["option"], text="Undergraduate", value="undergraduate")
 print(tags)
