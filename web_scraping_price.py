3c57dd1d1bd1f379556b5b7467913a38af039bfe
Chanchai Thaiyanon <chanchai.thaiyanon@outlook.com>
2024-02-22 20:29:52 +0700

    get price

--- web_scraping_price.py
+++ web_scraping_price.py
@@ -4,4 +4,11 @@ import requests
 url = "https://www.advice.co.th/product/notebook/notebook-acer/notebook-acer-travelmate-tmp216-51-576q-t006-steel-gray-"
 
 result = requests.get(url)
-print(result.text)
+doc = BeautifulSoup(result.text, "html.parser")
+# print(doc.prettify())
+
+# get price
+prices = doc.find_all(text="ราคาหน้าร้าน")
+parent = prices[0].parent.parent
+price = parent.text.split(" ")[2]
+print(price)
